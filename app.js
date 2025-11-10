2e9372221187612867b5ffca166614f57ff0d826
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-10 07:50:03 -0500

    (task 3): create object variable to use in function, added conditional to alert if not all inputs are completed

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ window.addEventListener("load", function(){
     buttonEl.addEventListener("click", function(){
         // define variables for the input fields
         let taskEl = document.getElementById("taskName");
-        let taskName = taskEl.value
+        let taskInput = taskEl.value
         let categoryEl = document.getElementById("category");
-        let category = categoryEl.value;
+        let categoryInput = categoryEl.value;
         let deadlineEl = document.getElementById("deadline");
-        let deadline = deadlineEl.value;
+        let deadlineInput = deadlineEl.value;
         let statusEl = document.getElementById("status");
-        let status = statusEl.value;
+        let statusInput = statusEl.value;
+
+        // define a general task object
+        let taskObj = {
+            task: taskInput,
+            cat: categoryInput,
+            date: deadlineInput,
+            stat: statusInput
+        };
+
         // add object to array if they are all filled
-        
+        if (taskInput === '' || categoryInput === '' || deadlineInput === '' || statusInput == ''){
+            alert("Fill in all fields");
+            return console.log('User must input all fields');
+        }
+
     });
 });
